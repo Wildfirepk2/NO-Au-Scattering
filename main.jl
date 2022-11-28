1c347009079d767dceef7a08cf9e9bc81ea81895
Wildfirepk2 <btang7@vols.utk.edu>
2022-11-28 17:32:19 -0500

    au 5000 step. need to fix

--- main.jl
+++ main.jl
@@ -45,7 +45,7 @@ const auatomcutoff=397
 const stepslogging=10
 
 ### scale down factor on steps for debugging. f=1 no scaling.
-const scalefactor=1000
+const scalefactor=1
 
 # actual steps for au equilibration. maybe edit later to always be divisable by 10
 const steps_eq::Int64=param.Nsteps_eq[1]/scalefactor
