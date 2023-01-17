36f5dfac05c4f117ee1cc4ae8eee2ace4a9fb3d9
Wildfirepk2 <btang7@vols.utk.edu>
2023-01-16 19:23:11 -0500

    restructure run slab/fetch eq coords

--- functions/SimAnalysisFunc.jl
+++ functions/SimAnalysisFunc.jl
@@ -376,10 +376,49 @@ end
 ############################################################################################################
 
 """
-run Au slab equilibration and output run info to results folder
+run Au slab equilibration and output run info to results folder IF Au slab is not already equilibrated
 """
 function runAuSlabEquilibration()
-    t=@elapsed include("functions/au slab equilibration.jl")
-    println("Au slab is equilibrated")
-    println("Time to run: $t seconds")
+    i_au=first(getAuDirIdx("results"))
+    if i_au isa Nothing
+        t=@elapsed include("functions/au slab equilibration.jl")
+        println("Au slab is equilibrated")
+        println("Time to run: $t seconds")
+    end
+end
+
+############################################################################################################
+
+"""
+helper function: get index of au folder in directory
+"""
+function getAuDirIdx(path::String=".")
+    resultsinpath=readdir(path;join=true)
+    i_au=findfirst(contains.(resultsinpath,"Au slab"))
+    return i_au, resultsinpath
+end
+
+############################################################################################################
+
+"""
+helper function: get path of au folder in directory
+"""
+function getAuDirPath(path::String=".")
+    i_au,resultsinpath=getAuDirIdx(path)
+    resultsinpath[i_au]
+end
+
+############################################################################################################
+
+"""
+get equilibrated au coords
+"""
+function getEquilAuCoords()
+    audir=getAuDirPath("results")
+    coordsfile="$audir/syscoords.xlsx"
+    xfcoord=XLSX.readxlsx(coordsfile)
+    sheets=XLSX.sheetnames(xfcoord)
+    sheetlastcoord=sheets[end]
+    dfcoord=DataFrame(XLSX.readtable(coordsfile,sheetlastcoord))
+    [SA[dfcoord[i,1],dfcoord[i,2],dfcoord[i,3]] for i in 1:nrow(dfcoord)]
 end
--- main.jl
+++ main.jl
@@ -73,26 +73,10 @@ NTRAP = 0
 ############################################################################################################
 
 # check if Au slab is equilibrated. if not, equilibrate Au slab
-
-# make helper function for outputting index of au folder. adapt for outputting most recent run?
-resultsdir=readdir("results";join=true)
-i_au=findfirst(contains.(resultsdir,"Au slab"))
-
-# leave in main
-if i_au isa Nothing
-    runAuSlabEquilibration()
-end
+runAuSlabEquilibration()
 
 # make file for holding no/au related functions
 # make function for no/au system init. function composed initNO and initeqAu
-resultsdir=readdir("results";join=true)
-i_au=findfirst(contains.(resultsdir,"Au slab"))
-audir=resultsdir[i_au]
-coordsfile="$audir/syscoords.xlsx"
-xfcoord=XLSX.readxlsx(coordsfile)
-sheets=XLSX.sheetnames(xfcoord)
-sheetlastcoord=sheets[end]
-dfcoord=DataFrame(XLSX.readtable(coordsfile,sheetlastcoord))
-aueqcoords=[SA[dfcoord[i,1],dfcoord[i,2],dfcoord[i,3]] for i in 1:nrow(dfcoord)]
+aueqcoords=getEquilAuCoords()
 
 # make simulator/system variables for no/au in global. later on put in separate file like au slab equilibration
