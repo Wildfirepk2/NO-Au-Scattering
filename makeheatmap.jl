32523e46d057275425f90dd9c81790f9a94f0d3c
Wildfirepk2 <btang7@vols.utk.edu>
2023-04-20 18:51:43 -0400

    prelim heatmap 3

--- makeheatmap.jl
+++ makeheatmap.jl
@@ -10,11 +10,8 @@ using XLSX
 using DataFrames
 using CSV
 using CairoMakie
-# using Unitful
 using Dates
 using StatsBase
-using LinearAlgebra
-# include("functions/MDUnits.jl")
 
 ############################################################################################################
 
@@ -53,10 +50,20 @@ function getfilteredAu(T)
     df_filtered = filter(row -> row.z > 6.5, df) # jump in z from ~5 to ~7
     # remove z col 
     df_filtered = select(df_filtered, Not(:z))
-    return df_filtered # should have 132 rows (528 atoms/4 layers)
+    convert.(Float64, df_filtered) # should have 132 rows (528 atoms/4 layers)
 end
 
-function filtertrajxy(df::DataFrame)
+function filtertrajxy(df::DataFrame,temp,orient)
+    e_filter=minimum(df.Ei)
+    if "θorient" in names(df)
+        # filter by orient and e_filter
+        filter!(row -> row.θorient == orient && row.Ei == e_filter, df)
+        var="θ$orient"
+    else
+        # filter by temp and e_filter
+        filter!(row -> row.T == temp && row.Ei == e_filter, df)
+        var="T$temp"
+    end
     # delete cols not matching "xcom" or "ycom"
     select!(df, "xcom", "ycom")
 
@@ -73,6 +80,11 @@ function makeresultsfolder(desc::String)
     println("Made folder: $path")
     return path
 end
+
+xlims(ax::Axis=current_axis()) =
+    (ax.finallimits[].origin[1], ax.finallimits[].widths[1])
+ylims(ax::Axis=current_axis()) =
+    (ax.finallimits[].origin[2], ax.finallimits[].widths[2])
 
 ############################################################################################################
 
@@ -93,30 +105,41 @@ t=@elapsed begin
     file_names = ["traj_scattered.xlsx", "traj_trapped.xlsx"]
     filetypes=["sc", "tr"]
 
+    ts=[300, 400, 500]
+    orients=[0, 180, 90]
+
     # dftraj=[[DataFrame() for _ in file_names] for _ in folder_names] # 1st vec is noau_norm/2nd vec is noau_fix/3rd vec is oau
 
     path=makeresultsfolder("heatmaps")
     # for (folderv,dfv) in zip(filtered_folders,dftraj), folder in folderv, (df,file) in zip(dfv,file_names)
-    for folderv in filtered_folders, (runtype,folder) in zip(runtypes,folderv), (filetype,file) in zip(filetypes,file_names)
+    for folderv in filtered_folders, (runtype,folder) in zip(runtypes,folderv), (filetype,file) in zip(filetypes,file_names)# , (t,orient) in zip(ts,orients)
         target_file = folder*"/"*file
         if isfile(target_file)
             try
                 # plot au coords
                 f = Figure(resolution = (800, 800))
-                Axis(f[1,1],xlabel="x (Å)",ylabel="y (Å)")
-                scatter!(df_au.x,df_au.y,color=:gold,markersize=50)
+                ax=Axis(f[1,1],xlabel="x (Å)",ylabel="y (Å)")
+                xylim=[(-1.5, 34),(-0.5, 30.5)]#\fix. unknown error giving ax limits as (0,10). happens only if fig gen in loop
+                nbin=30
+                xyedge=Tuple(range(xy...,nbin) for xy in xylim)
+                limits!(ax,xylim...)
+                scatter!(df_au.x,df_au.y,
+                        color=:gold,
+                        markersize=50,
+                        )
 
                 # overlay heatmap
                 data = DataFrame(XLSX.readtable(target_file, 1))
-                filtered_data=filtertrajxy(data)
+                filtered_data=filtertrajxy(data,t,orient)#\fix
                 # CSV.write("filtered_data.csv",filtered_data) #\tmp
                 # println(filtered_data) #\tmp
-                h = fit(Histogram, (filtered_data.x,filtered_data.y),nbins=50)
+                h = fit(Histogram, (filtered_data.x,filtered_data.y),xyedge)
                 heatmap!(h,
                         # axis = (;xlabel="x (Å)",ylabel="y (Å)"),
-                        colormap=:Reds,
+                        colormap=(:Reds,0.6),
                         interpolate=false,
-                        transparency = true,)
+                        # transparency = true,
+                        )
 
                 save("$path/heatmap-$runtype-$filetype-T$T.png",f)
                 break
@@ -124,6 +147,7 @@ t=@elapsed begin
                 cd(curdir)
                 error("Error occurred: $e")
             end
+            
             # append!(df, filtered_data)
         end
     end
