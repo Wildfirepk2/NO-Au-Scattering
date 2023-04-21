7b56cad658dd1f176a621f140c4eb97c54999a0d
Wildfirepk2 <btang7@vols.utk.edu>
2023-04-21 02:49:06 -0400

    heatmap

--- makeheatmap.jl
+++ makeheatmap.jl
@@ -1,7 +1,6 @@
 # Define directory path
 # dir_path = expanduser("~/scratch/NO-Au-results/run-4-13")
 dir_path = expanduser("~/Downloads/test_combine_excel")
-T=300 # temperature of heatmaps
 curdir=pwd()
 
 ############################################################################################################
@@ -26,7 +25,7 @@ function getAuDirPath(T)
 end
 
 """
-get equilibrated au coords from previous run
+get equilibrated au coords from run at temp T
 """
 function getEquilAuCoords(T)
     audir=getAuDirPath(T)
@@ -58,11 +57,11 @@ function filtertrajxy(df::DataFrame,temp,orient)
     if "θorient" in names(df)
         # filter by orient and e_filter
         filter!(row -> row.θorient == orient && row.Ei == e_filter, df)
-        var="θ$orient"
+        # var="θ$orient"
     else
         # filter by temp and e_filter
         filter!(row -> row.T == temp && row.Ei == e_filter, df)
-        var="T$temp"
+        # var="T$temp"
     end
     # delete cols not matching "xcom" or "ycom"
     select!(df, "xcom", "ycom")
@@ -71,7 +70,10 @@ function filtertrajxy(df::DataFrame,temp,orient)
     rename!(df, "xcom"=>"x", "ycom"=>"y")
 
     # change col types
-    convert.(Float64, df)
+    df_new=convert.(Float64, df)
+
+    # return df_new, var
+    return df_new
 end
 
 function makeresultsfolder(desc::String)
@@ -81,6 +83,45 @@ function makeresultsfolder(desc::String)
     return path
 end
 
+function outputgraph(dfvec,systype,temp,orient)
+    # get au top layer xy coords
+    df_au=getfilteredAu(temp)
+
+    # plot au coords
+    f = Figure(resolution = (800, 600))
+    ax=Axis(f[1,1],xlabel="x (Å)",ylabel="y (Å)")
+    xylim=[(-1.5, 34),(-0.5, 30.5)]#\fix. unknown error giving ax limits as (0,10). happens only if fig gen in loop
+    limits!(ax,xylim...)
+    scatter!(df_au.x,df_au.y,
+            color=:gold,
+            markersize=50,
+            )
+
+    # overlay heatmap
+    nbin=30
+    xyedge=Tuple(range(xy...,nbin) for xy in xylim)
+    colors=[:Reds, :Blues]
+    colorbarlabels=["Scattered", "Trapped"]
+    transparancy_fac=count(!isempty, dfvec)==1 ? 0.7 : 0.45 # may \fix
+    # figobjs=[]
+    for (df,color,label) in zip(dfvec,colors,colorbarlabels)
+        if !isempty(df)
+            h = fit(Histogram, (df.x,df.y),xyedge)
+            hm=heatmap!(h,
+                    colormap=(color,transparancy_fac),
+                    interpolate=false,
+                    # label=label,
+                    # transparency = true,
+                    )
+            Colorbar(f[:, end+1], hm, label=label,)
+        end
+    end
+
+    # save fig
+    var=contains(systype,"fix") ? "θ$orient" : "T$temp"
+    save("$path/heatmap-$systype-$var.png",f)
+end
+ 
 xlims(ax::Axis=current_axis()) =
     (ax.finallimits[].origin[1], ax.finallimits[].widths[1])
 ylims(ax::Axis=current_axis()) =
@@ -89,16 +130,12 @@ ylims(ax::Axis=current_axis()) =
 ############################################################################################################
 
 t=@elapsed begin
-    # get au top layer xy coords
-    df_au=getfilteredAu(T)
-
-    # get sc/tr xy coords for ea type
     cd(dir_path)
     dir_folders=readdir()
 
     # Define unique folder names
     folder_names = ["NO-Au_sc-ISAAC-normalrun", "NO-Au_sc-ISAAC-fixorient", "O-Au_sc-ISAAC-10000"]
-    runtypes=["noau_norm", "noau_fix", "oau"]
+    systypes=["noau_norm", "noau_fix", "oau"]
     filtered_folders = [filter(x -> contains(x, name), dir_folders) for name in folder_names]
 
     # Define unique file names
@@ -111,45 +148,32 @@ t=@elapsed begin
     # dftraj=[[DataFrame() for _ in file_names] for _ in folder_names] # 1st vec is noau_norm/2nd vec is noau_fix/3rd vec is oau
 
     path=makeresultsfolder("heatmaps")
-    # for (folderv,dfv) in zip(filtered_folders,dftraj), folder in folderv, (df,file) in zip(dfv,file_names)
-    for folderv in filtered_folders, (runtype,folder) in zip(runtypes,folderv), (filetype,file) in zip(filetypes,file_names)# , (t,orient) in zip(ts,orients)
-        target_file = folder*"/"*file
-        if isfile(target_file)
-            try
-                # plot au coords
-                f = Figure(resolution = (800, 800))
-                ax=Axis(f[1,1],xlabel="x (Å)",ylabel="y (Å)")
-                xylim=[(-1.5, 34),(-0.5, 30.5)]#\fix. unknown error giving ax limits as (0,10). happens only if fig gen in loop
-                nbin=30
-                xyedge=Tuple(range(xy...,nbin) for xy in xylim)
-                limits!(ax,xylim...)
-                scatter!(df_au.x,df_au.y,
-                        color=:gold,
-                        markersize=50,
-                        )
-
-                # overlay heatmap
+    # for (folderv,dfv) in zip(filtered_folders,dftraj), folder in folderv, (df,file) in zip(dfv,file_names)#\old
+    # iterate through each sys type, then each T OR orient
+    for (systype,folderv) in zip(systypes,filtered_folders), (t,orient) in zip(ts,orients)
+        # container for systype-T/orient combo
+        dfvec=[DataFrame() for _ in file_names]
+        # iterate through each folder corresponding to systype, then each type of file: tr and sc
+        for folder in folderv, (df,file) in zip(dfvec,file_names)
+            # add to corresponding df in dfvec
+            target_file = folder*"/"*file
+            if isfile(target_file)
                 data = DataFrame(XLSX.readtable(target_file, 1))
-                filtered_data=filtertrajxy(data,t,orient)#\fix
-                # CSV.write("filtered_data.csv",filtered_data) #\tmp
-                # println(filtered_data) #\tmp
-                h = fit(Histogram, (filtered_data.x,filtered_data.y),xyedge)
-                heatmap!(h,
-                        # axis = (;xlabel="x (Å)",ylabel="y (Å)"),
-                        colormap=(:Reds,0.6),
-                        interpolate=false,
-                        # transparency = true,
-                        )
-
-                save("$path/heatmap-$runtype-$filetype-T$T.png",f)
-                break
-            catch e
-                cd(curdir)
-                error("Error occurred: $e")
+                filtered_data=filtertrajxy(data,t,orient)
+                append!(df,filtered_data)
             end
-            
-            # append!(df, filtered_data)
         end
+        try
+            outputgraph(dfvec,systype,t,orient)
+            # break
+        catch e
+            cd(curdir)
+            error("Error occurred: $e")
+        end
+        # # save dfvec to file
+        # for i in eachindex(dfvec)
+        #     CSV.write("$path/$systype-$(filetypes[i])-T$t-θ$orient.csv",dfvec[i])
+        # end
     end
     cd(curdir)
 end
